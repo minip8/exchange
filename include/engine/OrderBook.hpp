1c5b850ff518f4f9030d6f47ca7f35ad50aaa31a
Matthew Yang <mattyang1340@gmail.com>
2026-06-07 21:34:28 +1000

    feat(OrderBook): initial header commit

--- include/engine/OrderBook.hpp
+++ include/engine/OrderBook.hpp
@@ -1,6 +1,24 @@
 #pragma once
 
+#include <optional>
+
+#include "engine/Order.hpp"
+#include "types/OrderId.hpp"
+namespace Engine {
+
 class OrderBook {
-  private:
-  
+ public:
+  OrderBook() noexcept;
+  OrderBook(const OrderBook&) = delete;
+  OrderBook& operator=(const OrderBook&) = delete;
+  OrderBook(OrderBook&&) noexcept;
+  OrderBook& operator=(OrderBook&&) noexcept;
+  ~OrderBook() noexcept;
+
+  std::optional<Order> addOrder(Order&&);
+  std::optional<Order> removeOrder(const OrderId);
+
+ private:
 };
+
+}  // namespace Engine
