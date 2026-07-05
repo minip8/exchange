2e52dad5c9bba976badb1cb971cb182392fa0bc3
Matthew Yang <mattyang1340@gmail.com>
2026-07-05 21:36:58 +1000

    feat(OrderQuantity): arithmetic operations

--- include/types/OrderQuantity.hpp
+++ include/types/OrderQuantity.hpp
@@ -11,5 +11,52 @@ struct OrderQuantity {
   explicit OrderQuantity(T v) : value(v) {}
 
   std::strong_ordering operator<=>(const OrderQuantity&) const = default;
+
+  // Quantity +/- Quantity
+  OrderQuantity& operator+=(const OrderQuantity& rhs) noexcept {
+    value += rhs.value;
+    return *this;
+  }
+  OrderQuantity& operator-=(const OrderQuantity& rhs) noexcept {
+    value -= rhs.value;
+    return *this;
+  }
+
+  // Quantity +/- raw offset
+  OrderQuantity& operator+=(T rhs) noexcept {
+    value += rhs;
+    return *this;
+  }
+  OrderQuantity& operator-=(T rhs) noexcept {
+    value -= rhs;
+    return *this;
+  }
+
+  // Scaling
+  OrderQuantity& operator*=(T rhs) noexcept {
+    value *= rhs;
+    return *this;
+  }
+  OrderQuantity& operator/=(T rhs) noexcept {
+    value /= rhs;
+    return *this;
+  }
 };
+
+inline OrderQuantity operator+(OrderQuantity lhs,
+                               const OrderQuantity& rhs) noexcept {
+  return lhs += rhs;
+}
+inline OrderQuantity operator-(OrderQuantity lhs,
+                               const OrderQuantity& rhs) noexcept {
+  return lhs -= rhs;
+}
+inline OrderQuantity operator*(OrderQuantity lhs,
+                               OrderQuantity::T rhs) noexcept {
+  return lhs *= rhs;
+}
+inline OrderQuantity operator/(OrderQuantity lhs,
+                               OrderQuantity::T rhs) noexcept {
+  return lhs /= rhs;
+}
 }  // namespace Exchange::Types
