05e2290ed41bd6a3892e1aec8341c4a8c4306d19
Matthew Yang <mattyang1340@gmail.com>
2026-07-05 21:39:14 +1000

    feat(MatchingEngine): processOrder

--- include/engine/MatchingEngine.hpp
+++ include/engine/MatchingEngine.hpp
@@ -1,5 +1,26 @@
 #pragma once
 
+#include <unordered_map>
+
+#include "engine/Order.hpp"
+#include "engine/OrderBook.hpp"
+#include "types/OrderId.hpp"
 namespace Exchange::Engine {
-class MatchingEngine {};
-}  // namespace Engine
+class MatchingEngine {
+ public:
+  MatchingEngine() noexcept = default;
+  MatchingEngine(const MatchingEngine&) = delete;
+  MatchingEngine& operator=(const MatchingEngine&) = delete;
+  MatchingEngine(MatchingEngine&&) noexcept = default;
+  MatchingEngine& operator=(MatchingEngine&&) noexcept = default;
+  ~MatchingEngine() noexcept = default;
+
+  void processOrder(Order order) {
+    OrderBook& order_book{m_id_to_book[order.id()]};
+    order_book.addOrder(std::move(order));
+  }
+
+ private:
+  std::unordered_map<OrderId, OrderBook> m_id_to_book;
+};
+}  // namespace Exchange::Engine
