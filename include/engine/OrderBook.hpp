f142df0b86ac321bc43a9d3257adef102cfccc5f
Matthew Yang <mattyang1340@gmail.com>
2026-07-07 00:09:34 +1000

    feat(OrderBook): matching logic

--- include/engine/OrderBook.hpp
+++ include/engine/OrderBook.hpp
@@ -1,14 +1,13 @@
 #pragma once
 
-#include <algorithm>
-#include <concepts>
+#include <functional>
 #include <optional>
 #include <vector>
 
+#include "engine/Fill.hpp"
 #include "engine/Order.hpp"
 #include "types/OrderBookId.hpp"
 #include "types/OrderId.hpp"
-#include "types/OrderQuantity.hpp"
 
 namespace Exchange::Engine {
 using namespace Exchange::Types;
@@ -23,54 +22,38 @@ class OrderBook {
 
   OrderBook(OrderBookId id) : m_id(id) {}
 
-  std::vector<Order> addOrder(Order) noexcept;
+  std::vector<Fill> addOrder(Order) noexcept;
   std::optional<Order> removeOrder(const OrderId&);
   std::span<const Order> buys() const noexcept { return m_buys; }
   std::span<const Order> sells() const noexcept { return m_sells; }
 
  private:
-  template <typename F>
-    requires std::predicate<F, const Order&, const Order&>
-  std::vector<Order> match(std::vector<Order>& left_orders,
-                           std::vector<Order>& right_orders, Order& left_order,
-                           F match_predicate);
+  std::vector<Fill> match(
+      std::vector<Order>& resting_orders, Order& aggressing_order,
+      std::function_ref<bool(Order& aggressing_order, Order& resting_order)>
+          match_predicate);
+
+  std::optional<Fill> match(
+      Order& aggressing_order, Order& resting_order,
+      std::function_ref<bool(Order& aggressing_order, Order& resting_order)>
+          match_predicate);
+
+ private:
+  using match_predicate_t =
+      std::function_ref<bool(Order& aggressing_order, Order& resting_order)>;
+
+  match_predicate_t m_match_buy_aggressor{
+      [](const Order& aggressing_order, const Order& resting_order) {
+        return aggressing_order.price >= resting_order.price;
+      }};
+  match_predicate_t m_match_sell_aggressor{
+      [](const Order& aggressing_order, const Order& resting_order) {
+        return aggressing_order.price >= resting_order.price;
+      }};
 
  private:
   OrderBookId m_id;
   std::vector<Order> m_buys{};
   std::vector<Order> m_sells{};
 };
-
-template <typename F>
-  requires std::predicate<F, const Order&, const Order&>
-std::vector<Order> OrderBook::match(
-    [[maybe_unused]] std::vector<Order>& left_orders,
-    std::vector<Order>& right_orders, Order& left_order, F match_predicate) {
-  std::vector<Order> filled_orders;
-  OrderQuantity quantity_matched{0};
-  long fully_filled_count{0};
-
-  while (!right_orders.empty() && left_order.quantity() > OrderQuantity{0}) {
-    Order& right_order = right_orders.front();
-    if (!match_predicate(left_order, right_order)) break;
-    OrderQuantity to_match{
-        std::min(left_order.quantity(), right_order.quantity())};
-    quantity_matched += to_match;
-    if (to_match == right_order.quantity()) {
-      filled_orders.emplace_back(std::move(right_order));
-    }
-    right_order.setQuantity(right_order.quantity() - to_match);
-
-    filled_orders.emplace_back(right_order.id(), right_order.price(),
-                               right_order.time(), to_match,
-                               right_order.side());
-
-    left_order.setQuantity(left_order.quantity() - to_match);
-    fully_filled_count += (right_order.quantity() == OrderQuantity{0});
-  }
-
-  right_orders.erase(right_orders.begin(),
-                     right_orders.begin() + fully_filled_count);
-  return filled_orders;
-}
 }  // namespace Exchange::Engine
--- src/engine/OrderBook.cpp
+++ src/engine/OrderBook.cpp
@@ -1,1 +1,94 @@
 #include "engine/OrderBook.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <optional>
+#include <vector>
+
+#include "engine/Fill.hpp"
+#include "engine/Order.hpp"
+#include "types/OrderQuantity.hpp"
+#include "types/OrderSide.hpp"
+
+namespace Exchange::Engine {
+/*
+Assumes that addOrder is called on orders that are in increasing order of time.
+*/
+std::vector<Fill> OrderBook::addOrder(Order aggressing_order) noexcept {
+  std::vector<Fill> fills{[&] {
+    switch (aggressing_order.side) {
+      case Types::OrderSide::Buy:
+        return match(m_sells, aggressing_order, m_match_buy_aggressor);
+      case Types::OrderSide::Sell:
+        return match(m_buys, aggressing_order, m_match_sell_aggressor);
+    }
+  }()};
+  if (aggressing_order.quantity > OrderQuantity{0}) {
+    switch (aggressing_order.side) {
+      case Types::OrderSide::Buy:
+        m_buys.insert(
+            std::ranges::upper_bound(m_buys, aggressing_order.price,
+                                     std::ranges::greater{}, &Order::price),
+            std::move(aggressing_order));
+
+      case Types::OrderSide::Sell:
+        m_buys.insert(
+            std::ranges::upper_bound(m_buys, aggressing_order.price,
+                                     std::ranges::less{}, &Order::price),
+            std::move(aggressing_order));
+    }
+  }
+  return fills;
+}
+std::vector<Fill> OrderBook::match(std::vector<Order>& resting_orders,
+                                   Order& aggressing_order,
+                                   match_predicate_t match_predicate) {
+  std::vector<Fill> fills{};
+  OrderQuantity quantity_matched{0};
+  long fully_filled_count{0};
+
+  while (!resting_orders.empty() &&
+         aggressing_order.quantity > OrderQuantity{0}) {
+    Order& resting_order =
+        resting_orders[static_cast<size_t>(fully_filled_count)];
+    std::optional<Fill> fill{
+        match(aggressing_order, resting_order, match_predicate)};
+    if (!fill.has_value()) break;
+    fills.push_back(std::move(fill).value());
+    fully_filled_count +=
+        static_cast<long>(resting_order.quantity == OrderQuantity{0});
+  }
+
+  resting_orders.erase(resting_orders.begin(),
+                       resting_orders.begin() + fully_filled_count);
+
+  return fills;
+}
+
+/*
+Assumes positive order quantity.
+Mutates the orders to reflect their updated quantities.
+*/
+std::optional<Fill> OrderBook::match(Order& aggressing_order,
+                                     Order& resting_order,
+                                     match_predicate_t match_predicate) {
+  if (match_predicate(aggressing_order, resting_order)) {
+    OrderQuantity quantity_to_match{
+        std::min(aggressing_order.quantity, resting_order.quantity)};
+
+    aggressing_order.quantity -= quantity_to_match;
+    resting_order.quantity -= quantity_to_match;
+
+    return Fill{
+        .resting_order_id = resting_order.id,
+        .aggressor_order_id = aggressing_order.id,
+        .aggressor_side = aggressing_order.side,
+        .price = resting_order.price,
+        .time = aggressing_order.time,
+        .quantity = quantity_to_match,
+    };
+  }
+  return {};
+}
+}  // namespace Exchange::Engine
