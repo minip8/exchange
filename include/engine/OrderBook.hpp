8ffd5f1cba0da060257ab62c1129b056bfd0658c
Matthew Yang <mattyang1340@gmail.com>
2026-07-17 11:11:55 +1000

    fix(OrderBook): type

--- include/engine/OrderBook.hpp
+++ include/engine/OrderBook.hpp
@@ -21,7 +21,7 @@ using namespace Exchange::Types;
 class OrderBook {
  private:
   using match_predicate_t =
-      std::function_ref<bool(Order& aggressing_order, Order& resting_order)>;
+      std::function_ref<bool(const Order& aggressing_order, const Order& resting_order)>;
 
   match_predicate_t m_match_buy_aggressor{
       [](const Order& aggressing_order, const Order& resting_order) {
